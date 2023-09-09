c0d1c60f844b15ac004ee8320b9518cbe1124d1d
Rostyslav <rostislavkpi@gmail.com>
2023-09-10 02:01:12 +0300

    Create board mutation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Trello Clone',
-  description: 'Trello clone build with Next.js and Tailwind CSS',
+  title: 'Trello clone',
+  description: 'Trello clone built with Next.js and Tailwind CSS',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export * from './board-card.component';
 export * from './boards-list.component';
+export * from './create-board.component';
+export * from './input.component';
 export * from './navbar.component';
 export * from './user-dropdown.component';
